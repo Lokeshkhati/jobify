8f9d6c218c2bb39f89069e20ed4041b2f367faad
Lokesh Khati <lokeshkhati005@gmail.com>
2022-11-29 22:40:53 +0530

    add new code

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ config()
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import 'express-async-errors'
+
+import authRouter from "./routes/authRoutes.js"
+import jobsRouter from "./routes/jobsRoutes.js"
 
 import notFoundMiddleware from "./middleware/not-found.js"
 import errorHandlerMiddleware from "./middleware/error-handler.js"
-
 
 app.use(express.json())
 app.use(cors({
@@ -19,6 +22,9 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
+
+app.use('api/v1/auth', authRouter)
+app.use('api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,20 @@
+import { StatusCodes } from "http-status-codes"
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err)
-    res.status(500).json({ msg: 'there was an error' })
+    const defaultError = {
+        statusCodes: err.statusCodes || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.msg || "Something went wrong,  try again later"
+    }
+    if (err.name === "ValidationError") {
+        defaultError.statusCodes = StatusCodes.BAD_REQUEST
+        defaultError.msg = Object.values(err.errors).map((error) => error.message).join(',')
+    }
+    if (err.code && err.code === 11000) {
+        defaultError.statusCodes = StatusCodes.BAD_REQUEST
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
+    }
+    // res.status(defaultError.statusCodes).json({ msg: err })
+    res.status(defaultError.statusCodes).json({ msg: defaultError.msg })
 }
 
 export default errorHandlerMiddleware
