d6531f5fefa77474359f5ab7b5973844a11ae14b
Lokesh Khati <lokeshkhati005@gmail.com>
2022-11-30 09:26:02 +0530

    setup token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,25 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
-
 import { BadRequestError } from "../errors/index.js"
 
 const register = async (req, res, next) => {
     const { name, email, password } = req.body
+
     if (!name || !email || !password) {
         throw new BadRequestError("Please provide all values")
     }
+    const UserExists = await User.findOne({ email })
 
-    const isUserExists = await User.findOne({ email })
-
-    if (isUserExists) {
+    if (UserExists) {
         throw new BadRequestError("Email already in use")
     }
+    const user = await User.create({ name, email, password })
 
-    const user = await User.create(name, lastName, email, password)
-    res.status(StatusCodes.OK).json({ user })
+    const token = user.createJWT()
+
+    res.status(StatusCodes.OK).json({ user: { email: user.email, name: user.name, lastName: user.lastName, location: user.location }, token, location: user.location })
 
 }
-
 const login = (req, res) => {
     res.send('login user')
 }
@@ -27,6 +27,5 @@ const login = (req, res) => {
 const updateUser = (req, res) => {
     res.send('update user')
 }
-
 
 export { register, login, updateUser }
