87693f27c14df6e3d2cc084bcf55929392579497
Lokesh Khati <lokeshkhati005@gmail.com>
2022-12-01 08:35:01 +0530

    add put method to update user profile

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,9 +51,27 @@ const login = async (req, res,) => {
     res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
-const updateUser = (req, res) => {
-    // console.log(req.user)
-    res.send('update user')
+const updateUser = async (req, res) => {
+    const { email, name, lastName, location } = req.body
+    if (!email || !name || !lastName || !location) {
+        throw new BadRequestError("Please provide all values")
+    }
+    const user = await User.findOne({ _id: req.user.userId })
+    user.email = email
+    user.name = name
+    user.lastName = lastName
+    user.location = location
+
+    await user.save()
+
+    const token = user.createJWT()
+
+    res.status(StatusCodes.OK).json({
+        user,
+        token,
+        location: user.location
+    })
+
 }
 
 export { register, login, updateUser }
