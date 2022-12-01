bc20c3fc5eb5af841ae798329a25d314b062a275
Lokesh Khati <lokeshkhati005@gmail.com>
2022-12-01 15:29:33 +0530

    updated job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,15 @@
-const createJob = (req, res) => {
-    res.send('create job')
+import { BadRequestError, UnauthenticatedError } from "../errors/index.js"
+import Job from "../models/Job.js"
+import { StatusCodes } from "http-status-codes"
+const createJob = async (req, res) => {
+
+    const { position, company } = req.body
+    if (!position || !company) {
+        throw new BadRequestError("Please Provide All Values")
+    }
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const deleteJob = (req, res) => {
