6ac284a84e9c518634d03f378e9bc70a9fbf5daa
Lokesh Khati <lokeshkhati005@gmail.com>
2022-12-02 16:44:54 +0530

    refactor

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,9 +1,13 @@
-import { BadRequestError, UnauthenticatedError } from "../errors/index.js"
+import { BadRequestError, NotFoundError } from "../errors/index.js"
 import Job from "../models/Job.js"
 import { StatusCodes } from "http-status-codes"
-const createJob = async (req, res) => {
+import checkPermissions from "../utils/checkPermissions.js"
+import mongoose from "mongoose"
+import moment from 'moment';
 
+const createJob = async (req, res) => {
     const { position, company } = req.body
+
     if (!position || !company) {
         throw new BadRequestError("Please Provide All Values")
     }
@@ -12,20 +16,106 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ job })
 }
 
-const deleteJob = (req, res) => {
-    res.send('delete job')
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({ createdBy: req.user.userId })
+    res.status(StatusCodes.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
 }
 
-const getAllJobs = (req, res) => {
-    res.send('get all jobs')
+const updateJob = async (req, res) => {
+    const { id: jobId } = req.params
+    const { company, position } = req.body
+    if (!position || !company) {
+        throw new BadRequestError('Please provide all values')
+    }
+    const job = await Job.findOne({ _id: jobId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    // check permissions
+    checkPermissions(req.user, job.createdBy)
+
+    const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(StatusCodes.OK).json({ updatedJob })
+
 }
 
-const updateJob = (req, res) => {
-    res.send('update job')
+const deleteJob = async (req, res) => {
+
+    const { id: jobId } = req.params
+
+    const job = await Job.findOne({ _id: jobId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    checkPermissions(req.user, job.createdBy)
+
+    await job.remove()
+    res.status(StatusCodes.OK).json({ msg: 'Success !, Job removed' })
 }
 
-const showStats = (req, res) => {
-    res.send('show stats')
+const showStats = async (req, res) => {
+    let stats = await Job.aggregate([
+        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+        { $group: { _id: '$status', count: { $num: 1 } } }
+    ])
+
+    stats = stats.reduce((acc, curr) => {
+        const { _id: title, count } = curr
+        acc[title] = count
+        return acc
+    }, {})
+
+    const defaultStats = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        declined: stats.declined || 0
+    }
+    let monthlyApplications = await Job.aggregate([
+        { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+        {
+            $group: {
+                _id: {
+                    year: {
+                        $year: '$createdAt',
+                    },
+                    month: {
+                        $month: '$createdAt',
+                    },
+                },
+                count: { $sum: 1 },
+            },
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 },
+    ]);
+
+    monthlyApplications = monthlyApplications
+        .map((item) => {
+            const {
+                _id: { year, month },
+                count,
+            } = item;
+            // accepts 0-11
+            const date = moment()
+                .month(month - 1)
+                .year(year)
+                .format('MMM Y');
+            return { date, count };
+        })
+        .reverse();
+
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
+
+
+
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats }
+
+
