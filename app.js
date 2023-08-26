1b3ba75df31f3c95648b0672913c7e5de3467ce8
lokesh <lokeshkhati005@gmail.com>
2023-08-26 05:53:48 +0530

    build production ready

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import morgan from 'morgan';
 import 'express-async-errors'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path'
 
 import authRouter from "./routes/authRoutes.js"
 import jobsRouter from "./routes/jobsRoutes.js"
@@ -15,10 +18,10 @@ import notFoundMiddleware from "./middleware/not-found.js"
 import errorHandlerMiddleware from "./middleware/error-handler.js"
 import isLoggedIn from "./middleware/auth.js"
 
-// if (process.env.NODE_ENV !== 'production') {
-//     app.use(morgan('dev'))
-// }
-
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
+const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.json())
 app.use(cors())
 
@@ -26,8 +29,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', isLoggedIn, jobsRouter)
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
